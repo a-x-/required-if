cdec0c01634598135245db02831b97f55e54c40c
Inventorem <me@invntrm.ru>
2014-08-18 11:49:34 +0400

    [Working][Alpha] Init module functionality.
    Support only if <radio button element selector> has [<radio button element attribute>] conditions.
    radio button is the factorElement
    
     * @todo add remap feature (re-traversal targets, perform add/del
     * operations on demand)
     * @todo add another conditions types support
     * @todo add another factorElements types support

--- required-if.js
+++ required-if.js
@@ -4,6 +4,76 @@
  * License: MIT
  * Author: a-x-
  */
-(function(){
 
+/**
+ * @todo add remap feature (re-traversal targets, perform add/del operations on demand)
+ * @todo add another conditions types support
+ * @todo add another factorElements types support
+ */
+(function () {
+    var targets = [].slice.call(document.querySelectorAll('[data-required]')),
+        assignObjectIfUndefined = function(object,property) {
+            return object[property] || (object[property] = {});
+        },
+        conditionMap = {};
+    //
+    // Initial targets traversal
+    targets.forEach(function (target, i) {
+        var descriptor = target.dataset.required.match(/^if\s+(.*)\s+has\s+(.*)$/i),
+            factorElement,
+            factorElementSelector,
+            condition;
+        if (descriptor === null) {
+            return;
+        }
+        factorElementSelector = descriptor[1];
+        factorElement = document.querySelector(factorElementSelector);
+        condition = descriptor[2];
+        //
+        // Mark target for futher reasons
+        target.classList.add('requiredif-target');
+        target.dataset.requiredifTargetId = i + 1;
+        //
+        // Parse [attribute] syntax
+        (function () {
+            console.log('Parse [attribute] syntax');
+            var conditionDescriptor = condition.match(/^\[([a-z\-]+)\]$/i),
+                conditionAttribute;
+            if (conditionDescriptor !== null) {
+                conditionAttribute = conditionDescriptor[1];
+                //
+                // Store current condition value
+                target.required
+                    = assignObjectIfUndefined(conditionMap, target.dataset.requiredifTargetId)[factorElementSelector]
+                    = !!factorElement[conditionAttribute];
+                console.log(factorElementSelector, factorElement, factorElement[conditionAttribute], conditionAttribute, i + 1);
+                // Add listener to factorElement
+                if(factorElement.tagName.toLowerCase() === 'input' && factorElement.type.toLowerCase() === 'radio') {
+                    document.querySelectorAll('[name="'+factorElement.name+'"]').addEventListener('change', function (event) {
+                        // Attention!: Lisp pasta...
+                        if (
+                                // factorElement has specified attribute
+                            (factorElement[conditionAttribute])
+                                === //
+                                // previous condition value is false
+                                ((conditionMap[target.dataset.requiredifTargetId] || [])[factorElementSelector] === false)
+                            ) {
+                            target.required
+                                = conditionMap[target.dataset.requiredifTargetId][factorElementSelector]
+                                = (factorElement[conditionAttribute]);
+                        }
+                    });
+                }
+                else if (false) {
+                    // todo another element processor
+                }
+                else {
+                    throw 'unknown element type';
+                }
+                return true;
+            }
+            return false;
+        }());
+    });
+    console.log({conditionMap:conditionMap, targets:targets});
 }());
